8b7e974daadb2e4798550fe62e0d647bded2887a
lucasjdc <lucasjpdc@gmail.com>
2025-05-23 20:48:01 -0300

    feat: adiciona modelo Personalidade e rota /personalidades

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/lucasjdc/api-go-rest/models"
 	"github.com/lucasjdc/api-go-rest/routes"
 )
 
 func main() {
+
+	models.Personalidades = []models.Personalidade {
+		{Nome: "Cacau", Historia: "Historia 1"},
+		{Nome: "Fiona", Historia: "Historia 2"},
+		{Nome: "Luna", Historia: "Historia 3"},
+		{Nome: "Rex", Historia: "Historia 3"},
+	}
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleResquest()
 }
